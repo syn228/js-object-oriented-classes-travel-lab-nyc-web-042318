d158ee96a96d9edb0fec89ed32a9f61fdc979b70
Aaron Na <syn228@nyu.edu>
2018-06-05 18:48:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear()
+  }
+}
 
+
+
+class Route{
+  constructor(start, end){
+    this.start = start;
+    this.end = end
+  }
+  blocksTravelled(){
+    if (this.start.horizontal === 'Park'){
+      this.start.horizontal = 5;
+    };
+    if (this.end.horizontal === 'Park'){
+      this.end.horizontal = 5;
+    };
+    let v = Math.abs(parseInt(this.start.vertical) - parseInt(this.end.vertical));
+    let h = Math.abs(parseInt(this.start.horizontal) - parseInt(this.end.horizontal));
+    return h+v;
+  }
+  estimatedTime(boolean){
+    let a = this.blocksTravelled()
+    if (boolean){
+      return a/2
+    } else
+    return a/3
+  }
+}
